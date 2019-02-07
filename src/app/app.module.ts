ed295ca4823c294e8d05913bb7dc47465766433d
ElBatu <crpalacio52@gmail.com>
2019-02-06 22:10:52 -0500

    Template listo sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MenuLeftComponent } from './shared/menu-left/menu-left.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuLeftComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
